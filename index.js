c58f95315b0bf23d963ab8952e3429cf87e8f006
fcalabria <pcalabria@gmail.com>
2018-01-06 19:58:53 +0100

    Functional publish command + small functions sorting

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Twitter = require('twitter');
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {
   polling: true,
 });
+
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -13,12 +14,19 @@ const client = new Twitter({
 });
 
 const forceReply = { reply_markup: { force_reply: true } };
+const markdown = { parse_mode: 'Markdown' };
+
+function getRandomElementOn(array) {
+  const randomId = Math.floor(Math.random() * (array.length - 2));
+  return array[randomId];
+}
 
 function filterByAuthor(tweets, author) {
   return tweets.filter(tweet => tweet.entities.hashtags
     .map(hastag => hastag.text.toLowerCase())
     .includes(author));
 }
+
 function getRandomTweet(tweets, from) {
   let filteredTweets;
   switch (from) {
@@ -35,8 +43,7 @@ function getRandomTweet(tweets, from) {
   if (filteredTweets.length === 0) {
     return null;
   }
-  const randomId = Math.floor(Math.random() * (filteredTweets.length - 2));
-  const tweet = filteredTweets[randomId];
+  const tweet = getRandomElementOn(filteredTweets);
   return {
     id: tweet.id_str,
     text: tweet.text,
@@ -44,11 +51,6 @@ function getRandomTweet(tweets, from) {
   };
 }
 
-function responseTweet(chatId, tweet) {
-  const date = tweet.date.toLocaleDateString();
-  const text = `${tweet.text}\n[${date}](https://twitter.com/LaAfrolatina/status/${tweet.id})`;
-  bot.sendMessage(chatId, text, { parse_mode: 'Markdown' });
-}
 function getBunchOfTweets(fromId) {
   return new Promise((resolve) => {
     client.get('statuses/user_timeline', { userId: '3420274348', count: 200, max_id: fromId }, (error, params, response) => {
@@ -71,7 +73,28 @@ async function getAllTweets() {
   } while (lastBunch.length === 200);
   return allTweets;
 }
+
+function responseTweet(chatId, tweet) {
+  const date = tweet.date.toLocaleDateString();
+  const text = `${tweet.text}\n[${date}](https://twitter.com/LaAfrolatina/status/${tweet.id})`;
+  bot.sendMessage(chatId, text, markdown);
+}
+
+function randomLoadingMsg(chatId) {
+  const messages = [
+    'Ok, voy',
+    'Mmmmmm',
+    'Veamos...',
+    'Estoy en ello',
+    'Ahoy, mate!',
+    '[insert spinning circle]',
+  ];
+  const msg = getRandomElementOn(messages);
+  bot.sendMessage(chatId, msg);
+}
+
 async function getTweetFlow(chatId, fallbackMsg, from) {
+  randomLoadingMsg(chatId);
   try {
     const allTweets = await getAllTweets();
     const tweet = getRandomTweet(allTweets, from);
@@ -85,29 +108,18 @@ async function getTweetFlow(chatId, fallbackMsg, from) {
   }
 }
 
-bot.onText(/\/random/, (msg) => {
-  getTweetFlow(msg.chat.id, 'Error chungo :S');
-});
-
-bot.onText(/\/afrolatina/, (msg) => {
-  getTweetFlow(msg.chat.id, 'La afrolatina está poco habladora hoy', 'afrolatina');
-});
-
-bot.onText(/\/guest/, (msg) => {
-  bot.sendMessage(msg.chat.id, 'Ok, ¿de quién?', forceReply)
-    .then((textResponse) => {
-      bot.onReplyToMessage(textResponse.chat.id, textResponse.message_id, (searchForResponse) => {
-        const author = searchForResponse.text.toLowerCase().replace('#', '');
-        getTweetFlow(msg.chat.id, 'Mmmm... no encuentro. ¿Has probado con artistaInvitado?', author);
-      });
-    });
-});
-
 function publishTweet(tweet, chatId) {
   const fullTweet = tweet.hastag
     ? `"${tweet.text}" #${tweet.hastag}`
     : tweet.text;
-  bot.sendMessage(chatId, `Tweet para publicar: ${fullTweet}`);
+  client.post('statuses/update', { status: fullTweet }, (error, params, response) => {
+    if (error) {
+      console.log(error);
+      bot.sendMessage(chatId, 'Ups, algo ha fallado');
+    }
+    const publishedTweet = JSON.parse(response.body);
+    bot.sendMessage(chatId, `[¡Hecho!](https://twitter.com/LaAfrolatina/status/${publishedTweet.id_str})`, markdown);
+  });
 }
 
 function isAfrolatina(chatId) {
@@ -131,13 +143,31 @@ function getAuthorAndPublish(chatId, tweet) {
       });
     });
 }
+
+bot.onText(/\/random/, (msg) => {
+  getTweetFlow(msg.chat.id, 'Error chungo :S');
+});
+
+bot.onText(/\/afrolatina/, (msg) => {
+  getTweetFlow(msg.chat.id, 'La afrolatina está poco habladora hoy', 'afrolatina');
+});
+
+bot.onText(/\/guest/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'Ok, ¿de quién?', forceReply)
+    .then((textResponse) => {
+      bot.onReplyToMessage(textResponse.chat.id, textResponse.message_id, (searchForResponse) => {
+        const author = searchForResponse.text.toLowerCase().replace('#', '');
+        getTweetFlow(msg.chat.id, 'Mmmm... no encuentro. ¿Has probado con artistaInvitado?', author);
+      });
+    });
+});
+
 bot.onText(/\/publish/, (msg) => {
   const chatId = msg.chat.id;
   const tweet = {
     text: '',
     hastag: '',
   };
-  bot.sendMessage(chatId, '¡Frase nueva!¡Guay!');
   bot.sendMessage(chatId, '¿Qué han dicho?', forceReply)
     .then((textResponse) => {
       bot.onReplyToMessage(textResponse.chat.id, textResponse.message_id, (sentenceMsg) => {
--- readme.md
+++ readme.md
@@ -1,8 +1,14 @@
 # Afrolatina Bot
+
 > https://twitter.com/@laafrolatina
 
 ## Commands
+
 - **/random** gets a random tweet from the account
 - **/afrolatina** gets a random tweet said by the original afrolatina
 - **/guest** gets a random tweet from someone else
 - **/publish** to add a new quote in the @laafrolatina account
+
+## Known issues
+
+- Date is displayed as year/month/day instead of day/month/year
